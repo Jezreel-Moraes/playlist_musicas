261a9a74f1cea9054b872cfa39a8b0734755d184
Jezreel Kintino de Moraes <69700303+Jezreel-Moraes@users.noreply.github.com>
2023-06-28 17:34:12 -0300

    Inserindo dados

--- functions.c
+++ functions.c
@@ -12,12 +12,51 @@
 boolean isValidOption(int option) { return option > 0 && option < 10; }
 void invalidOption() { message("Opcao invalida!"); }
 
-void insert(char *record) {
+void populateRecordString(char *record, char *field, int *currentIndex) {
+  for (int i = 0; field[i] != '\0'; i++) {
+    record[(*currentIndex)++] = field[i];
+  }
+  record[(*currentIndex)++] = ';';
+}
+
+char *musicToString(struct Music *music) {
+  char *record = malloc(sizeof(char) * 250);
+  char *intToStr = malloc(sizeof(char) * 10);
+  int currentIndex = 0;
+
+  populateRecordString(record, music->name, &currentIndex);
+
+  sprintf(intToStr, "%d", music->duration);
+  populateRecordString(record, intToStr, &currentIndex);
+
+  populateRecordString(record, music->style, &currentIndex);
+  populateRecordString(record, music->artist.name, &currentIndex);
+  populateRecordString(record, music->artist.nationality, &currentIndex);
+
+  sprintf(intToStr, "%d", music->registrationDate.day);
+  populateRecordString(record, intToStr, &currentIndex);
+
+  sprintf(intToStr, "%d", music->registrationDate.month);
+  populateRecordString(record, intToStr, &currentIndex);
+
+  sprintf(intToStr, "%d", music->registrationDate.year);
+  populateRecordString(record, intToStr, &currentIndex);
+
+  record[currentIndex++] = '\n';
+  record[currentIndex] = '\0';
+  return record;
+}
+
+void insert(struct Music *music) {
   FILE *file = fopen(RECORDS_PATH, "at");
   if (file == NULL) {
     message("Erro ao inserir registro!");
     return;
   }
+
+  char *record = musicToString(music);
+
+  printf("record: %s\n", record);
 
   fputs(record, file);
   fclose(file);
@@ -47,18 +86,32 @@ int find(char *key) {
   return index;
 }
 
-void newRecord(struct Playlist *playlist) { insert("Banana\n"); }
+void newRecord(struct Playlist *playlist) {}
 
 void recordRemove(struct Playlist *playlist) {}
 
-void listRecords() {
-  clearScreen();
-  int i = find("Maracuja");
-  printf("Achado em i = %d", i);
-}
+void listRecords() {}
 
 void showRecord(struct Playlist *playlist) {}
 
 // Forma de selecionar a função baseado em posição
 const void (*FUNCTIONS[])() = {invalidOption, newRecord, recordRemove,
                                listRecords, showRecord};
+
+int main(int argc, char const *argv[]) {
+  struct Music *music = malloc(sizeof(struct Music));
+
+  strcpy(music->name, "Roberto Martins");
+  music->duration = 31;
+  strcpy(music->style, "Phonk");
+  strcpy(music->artist.name, "Cordel");
+  strcpy(music->artist.nationality, "Russo");
+  music->registrationDate.day = 26;
+  music->registrationDate.month = 6;
+  music->registrationDate.year = 2023;
+
+  insert(music);
+  printf("Inserido");
+  free(music);
+  return 0;
+}
