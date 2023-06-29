9ce454c2fd028e2c9db883b28086a19db271bd2b
Gustavo Henrique da Trindade Martins <103002928+GustavoHTM@users.noreply.github.com>
2023-06-29 14:32:20 -0300

    validacoes feitas

--- functions.c
+++ functions.c
@@ -9,7 +9,7 @@
 #include "globals.c"
 #include "menu.c"
 
-boolean isValidOption(int option) { return option > 0 && option < 10; }
+bool isValidOption(int option) { return option > 0 && option < 10; }
 void invalidOption() { message("Opcao invalida!"); }
 
 void populateRecordString(char *record, char *field, int *currentIndex) {
@@ -50,13 +50,13 @@ char *musicToString(struct Music *music) {
 void insert(struct Music *music) {
   FILE *file = fopen(RECORDS_PATH, "at");
   if (file == NULL) {
-    message("Erro ao inserir registro!");
+    message("\nErro ao inserir registro!");
     return;
   }
 
   char *record = musicToString(music);
 
-  printf("record: %s\n", record);
+  printf("\nrecord: %s\n", record);
 
   fputs(record, file);
   fclose(file);
@@ -69,21 +69,73 @@ void getStringInput(char *prompt, char *atr) {
 }
 
 void getIntegerInput(char *prompt, int *atr) {
-  printf("\n\n%s", prompt);
-  scanf("%d", atr);
-  fflush(stdin);
+  char *aux;
+  bool validator;
+  do {
+    printf("\n\n%s", prompt);
+    fgets(aux, 49, stdin);
+    aux[strcspn(aux, "\n")] = '\0';
+    validator = isNumber(aux);
+
+    if (!validator) message("\nFormato invalido, tente novamente...");
+  } while (!validator);
+
+  atr = stringToInteger(aux);
 }
 
-int isValidDate(int days, int month, int year) {
+int isValidDate(struct Date date) {
   int daysForMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-  if (year > 2023) return 0;
+  if (date.year > 2023) return 0;
 
-  if (month < 1 || month > 12) return 0;
+  if (date.month < 1 || date.month > 12) return 0;
 
-  if (days < 1 || days > daysForMonth[month - 1]) return 0;
+  if (date.day < 1 || date.day > daysForMonth[date.month - 1]) return 0;
 
   return 1;
+}
+
+bool isBlank(char *str) {
+  while (*str != '\0') {
+    if (!isspace(*str)) {
+      return false;
+    }
+    str++;
+  }
+  return true;
+}
+
+void enterStringInput(char *prompt, char *atr) {
+  bool validator;
+  do {
+    clearScreen();
+    getStringInput(prompt, atr);
+    validator = isBlank(atr);
+
+    if (validator) message("\nO campo nao pode ser vazio, tente novamente...");
+  } while (validator);
+}
+
+bool isNumber(const char *str) {
+  int i = 0;
+  while (str[i] != '\0') {
+    if (str[i] < '0' || str[i] > '9') return false;
+    i++;
+  }
+
+  return true;
+}
+
+void dateInput(struct Date date) {
+  bool validator;
+
+  do {
+    printf("\n\nDigite a data no formato dd/mm/aaaa: ");
+    scanf("%d/%d/%d", &date.day, &date.month, &date.year);
+
+    validator = isValidDate(date);
+    if (!validator) message("\nFormato de data invalida, tente novamente...");
+  } while (!validator);
 }
 
 void newRecord() {
@@ -94,33 +146,29 @@ void newRecord() {
   }
 
   struct Music music;
-  bool musicDuplicated;
+  bool musicDuplicated, validator;
   int validDate;
 
   do {
     clearScreen();
     getStringInput("Informe o nome da musica: ", music.name);
     musicDuplicated = find(music.name) != -1;
+    validator = isBlank(music.name);
 
     if (musicDuplicated) message("\nMusica ja existente, tente novamente...");
-  } while (musicDuplicated);
+    if (validator) message("\nO campo nao pode ser vazio, tente novamente...");
+  } while (musicDuplicated || validator);
 
   getIntegerInput("Informe seu tempo de duracao em minutos: ", &music.duration);
 
-  getStringInput("Informe o estilo musical da musica: ", music.style);
+  enterStringInput("Informe o estilo musical da musica: ", music.style);
 
-  getStringInput("Informe o nome do artista: ", music.artist.name);
+  enterStringInput("Informe o nome do artista: ", music.artist.name);
 
-  getStringInput("Informe a nacionalidade do artista: ",
-                 music.artist.nationality);
+  enterStringInput("Informe a nacionalidade do artista: ",
+                   music.artist.nationality);
 
-  do {
-    printf("\n\nDigite a data no formato dia/mes/ano: ");
-    scanf("%d/%d/%d", &music.registrationDate.day,
-          &music.registrationDate.month, &music.registrationDate.year);
-
-    if (!validDate) message("\nData invalida, tente novamente...");
-  } while (!validDate);
+  dateInput(music.registrationDate);
 
   insert(&music);
   fclose(file);
